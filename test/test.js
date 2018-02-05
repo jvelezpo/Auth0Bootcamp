ad59f23a9965cc1d0bcbb775e918a89c119713b5
jvelezpo <sebasvelez@gmail.com>
2018-02-05 18:23:36 -0500

    add mocha and express

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('true', function() {
+  describe('true', function() {
+    it('should assert', function() {
+      assert.equal(true, true);
+    });
+  });
+});
