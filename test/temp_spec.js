2afc895e93d1137bd582a5d3d1eb702b78f8548f
jvelezpo <sebasvelez@gmail.com>
2018-02-06 08:57:14 -0500

    testing for /comparePassAndHash endpoint

--- test/temp_spec.js
+++ test/temp_spec.js
@@ -7,7 +7,7 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('App', function() {
-  describe('/encrypt?password=somevalue', function() {
+  describe('/encrypt', function() {
     it('responds with status 200', function(done) {
       chai.request(app)
         .get('/encrypt?password=somevalue')
@@ -22,6 +22,40 @@ describe('App', function() {
         .get('/encrypt?password=somevalue')
         .end(function(err, res) {
           expect(res.body.hash).to.have.lengthOf(60);
+          done();
+        });
+    });
+  });
+
+  describe('/comparePassAndHash', function() {
+    it('responds with a status 200', function(done) {
+      chai.request(app)
+        .get(`/comparePassAndHash?password=somevalue&hash=${Array(60).join('a')}`)
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('responds true with the correct hash', function(done) {
+      chai.request(app)
+        .get('/encrypt?password=somevalue')
+        .end(function(err, res) {
+          expect(res.body.hash).to.have.lengthOf(60);
+          chai.request(app)
+            .get(`/comparePassAndHash?password=somevalue&hash=${res.body.hash}`)
+            .end(function(err, res) {
+              expect(res.body.success).to.be.true;
+              done();
+            });
+        });
+    });
+
+    it('responds false with the incorrect hash', function(done) {
+      chai.request(app)
+        .get(`/comparePassAndHash?password=somevalue&hash=${Array(60).join('a')}`)
+        .end(function(err, res) {
+          expect(res.body.success).to.be.false;
           done();
         });
     });
