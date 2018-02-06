834059c86f0965d7e1abe7428eaa47cd11f16770
Manuel Marcatili <marca.manu2@gmail.com>
2018-02-06 19:01:33 -0300

    Added checkStrenght endpoint (#3)
    * Added endpoint and first test
    
    * Made change on the return line of checkStrenght method and added more tests
    
    * removed not used constants

--- test/temp_spec.js
+++ test/temp_spec.js
@@ -7,6 +7,68 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('App', function() {
+  describe('/checkStrenght', function() {
+    it('should fail on not having uppercase letter', function(done) {
+      chai.request(app)
+      .get('/checkStrenght?password=p@ssword123')
+      .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect('Content-Type', /json/);
+        expect(res.body.errors).to.include('The password must contain at least one uppercase letter.')
+        done();
+      })
+    });
+
+    it('should fail on not having special character', function(done) {
+      chai.request(app)
+      .get('/checkStrenght?password=Password123')
+      .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect('Content-Type', /json/);
+        expect(res.body.errors).to.include('The password must contain at least one special character.')
+        done();
+      })
+    });
+
+    it('should fail on not having lowercase letter', function(done) {
+      chai.request(app)
+      .get('/checkStrenght?password=PASSWORD123')
+      .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect('Content-Type', /json/);
+        expect(res.body.errors).to.include('The password must contain at least one lowercase letter.')
+        done();
+      })
+    });
+
+    it('should fail on not having one number', function(done) {
+      chai.request(app)
+      .get('/checkStrenght?password=PASSWORD')
+      .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect('Content-Type', /json/);
+        expect(res.body.errors).to.include('The password must contain at least one number.')
+        done();
+      })
+    });
+
+    it('should fail on not having having at least 8 chars', function(done) {
+      chai.request(app)
+      .get('/checkStrenght?password=PASS')
+      .end((err,res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect('Content-Type', /json/);
+        expect(res.body.errors).to.include('The password must be at least 8 characters long.')
+        done();
+      })
+    });
+  });
+
   describe('/encrypt', function() {
     it('responds with status 200', function(done) {
       chai.request(app)
